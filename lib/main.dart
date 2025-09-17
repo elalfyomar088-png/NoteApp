515ad6e04d79be3f760efe622f1964f8be6f5ca7
Mastar <mastar@example.com>
2025-09-17 14:09:57 +0300

    theme

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/views/notes_view.dart';
+void main() {
+  runApp(const  NotesApp());
+}
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NotesView(),
+    );
+  }
+}
