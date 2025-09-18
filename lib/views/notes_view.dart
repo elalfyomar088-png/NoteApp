c7b408a0c1ef3a93f476e516a84a76a2d7dddcb5
Mastar <mastar@example.com>
2025-09-18 16:28:52 +0300

    omar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child:const Icon(Icons.add),
+        ),
+      body:const NotesViewBody(),
+    );
   }
 }
---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/widgets/custom_app_bar.dart';
+import 'package:flutter_application_4/widgets/notes_list_view.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody
+  ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children:const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          Expanded(child: NotesListView(),),
+        ],
+      ),
+    );
+  }
+}
+
+
+
